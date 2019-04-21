8fda1a7fd853ee6ce5394ad20aff1a19623258ed
Jakson Lima <jaksonsneider@gmail.com>
2019-04-21 16:41:28 -0300

    Stream Part#2

---
+++ src/main/java/p/br/com/blue/jakson/streams/exemplo/dois/Produto.java
@@ -0,0 +1,72 @@
+package p.br.com.blue.jakson.streams.exemplo.dois;
+
+public class Produto {
+
+	private Integer id;
+	private String nome;
+	private Double preco;
+
+	public Produto() {
+	}
+
+	public Produto(Integer id, String nome, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", nome=" + nome + ", preco=" + preco + "]";
+	}
+
+}
