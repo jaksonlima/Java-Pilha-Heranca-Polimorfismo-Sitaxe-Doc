aebcc49f678313d883a527a512949d66c2d1cec8
Jakson Lima <jaksonsneider@gmail.com>
2019-04-23 21:38:39 -0300

    Criação de Threads Part #1

---
+++ src/main/java/q/br/com/blue/jakson/criando/threads/Main.java
@@ -0,0 +1,23 @@
+package q.br.com.blue.jakson.criando.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+	public static void main(String[] args) {
+
+		ExecutorService e0 = Executors.newSingleThreadExecutor();
+		ExecutorService e = Executors.newFixedThreadPool(5);
+
+		for (int i = 0; i <= 5; i++) {
+
+			Task t = new Task(i);
+
+			e.execute(t);
+		}
+
+		e.shutdown();
+
+	}
+
+}
