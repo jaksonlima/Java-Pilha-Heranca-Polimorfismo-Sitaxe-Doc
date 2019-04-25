26a2797f596d8c8b08d9b881324e450b9cf43750
Jakson Lima <jaksonsneider@gmail.com>
2019-04-24 21:29:58 -0300

    Threads ReenTranTreadWritelock Part#3

---
+++ src/main/java/s/br/com/blue/jakson/criando/threads/reentrantreadwritelock/Escritor.java
@@ -0,0 +1,17 @@
+package s.br.com.blue.jakson.criando.threads.reentrantreadwritelock;
+
+public class Escritor implements Runnable {
+
+	private Info info;
+
+	public Escritor(Info info) {
+		this.info = info;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			info.setInfo();
+		}
+	}
+}
---
+++ src/main/java/s/br/com/blue/jakson/criando/threads/reentrantreadwritelock/Leitor.java
@@ -0,0 +1,26 @@
+package s.br.com.blue.jakson.criando.threads.reentrantreadwritelock;
+
+public class Leitor implements Runnable {
+
+	private Info info;
+	private String nome;
+	
+	public Leitor(String nome, Info info) {
+		this.nome = nome;
+		this.info = info;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			for(int i = 0; i < 10; i++) {
+				String text = info.getInfo();
+				System.out.println(nome + " => leu: " + text);
+				Thread.sleep(100);
+			}
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
